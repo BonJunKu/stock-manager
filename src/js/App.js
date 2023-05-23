8a3c830a1c987f5fe38bc3d845bc367f7a0147bd
Bonjun Ku <ku96kbj@naver.com>
2023-05-24 02:10:46 +0900

    Fix delay time

--- src/js/App.js
+++ src/js/App.js
@@ -165,7 +165,7 @@ export default class App {
               },
             ],
           });
-        }, 500);
+        }, 300);
       }
 
       if (
@@ -234,7 +234,7 @@ export default class App {
               },
             ],
           });
-        }, 500);
+        }, 300);
       }
     });
 
